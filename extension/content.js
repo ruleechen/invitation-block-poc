fd235de64dfe65d67aa997e76591c09c41e25509
Rulee Chen <rulee@live.cn>
2018-12-13 00:27:18 +0800

    fix bugs

--- extension/content.js
+++ extension/content.js
@@ -1,27 +1,34 @@
 function appendHtml(el, html) {
-    var temp = document.createElement('div');
-    temp.style.display = 'none';
-    document.body.appendChild(temp);
-    temp.innerHTML = html;
-    while (temp.firstChild) {
-        el.appendChild(temp.firstChild);
-    }
-    document.body.removeChild(temp);
+  const temp = document.createElement('div');
+  temp.style.display = 'none';
+  temp.innerHTML = html;
+  while (temp.firstChild) {
+    el.appendChild(temp.firstChild);
+  }
+}
+
+function insertBefore(el, html) {
+  const temp = document.createElement('div');
+  temp.style.display = 'none';
+  temp.innerHTML = html;
+  while (temp.firstChild) {
+    el.parentNode.insertBefore(temp.firstChild, el);
+  }
 }
 
 const phoneHtml = `
 <div class="rc-phone">
-    <button class="rc-schedule-meeting">Schedule Meeting</button>
+    <button class="rc-schedule-invitation">Schedule Invitation</button>
     <br><br>
-    <button class="rc-clean-meeting">Clean Meeting</button>
+    <button class="rc-clean-invitation">Clean Invitation</button>
     <div class="rc-message"></div>
 </div>
 `;
 
 const meetingInvitation = `
-<div aria-label="ringcentral-meeting" dir="1234567" style="border:1px solid #aaaaaa;padding:0;background:#eeeeee;" contenteditable="false">
-    <div style="background:orange;padding:2px 10px;">RingCentral Meeting Invitation</div>
-    <div style="padding:10px">
+<div aria-label="ringcentral-invitation" dir="1234567" style="border:1px solid #aaaaaa;padding:0;background:#eeeeee;width:600px">
+    <div aria-label="ringcentral-invitation-header" style="background:orange;padding:2px 10px;">RingCentral Invitation</div>
+    <div aria-label="ringcentral-invitation-content" style="padding:10px">
         <p>Rulee is inviting you to a RingCentral meeting.</p>
         <p>Join from PC, Mac, iOS or Android: <a href="https://rcdev.zoom.us/j/1481393021">https://rcdev.zoom.us/j/1481393021</a></p>
         <br>
@@ -36,65 +43,123 @@ const meetingInvitation = `
 </div>
 `;
 
-appendHtml(document.body, phoneHtml);
+function initPOC() {
 
-let richEditor;
+  appendHtml(document.body, phoneHtml);
 
-function queryInvitationBlocks() {
-    return document.querySelectorAll('[aria-label="ringcentral-meeting"]');
-}
+  let richEditor;
 
-function initInvitationBlock(block) {
-    block.setAttribute('contenteditable', 'false');
-}
+  function queryInvitationBlocks() {
+    return document.querySelectorAll('[aria-label="ringcentral-invitation"]');
+  }
 
-const divMessage = document.querySelector('.rc-message');
-function showMessage(message) {
+  function cleanInvitationBlocks() {
+    const blocks = queryInvitationBlocks();
+    blocks.forEach((block) => {
+      block.parentNode.removeChild(block);
+    });
+    showMessage(`${blocks.length} invitation block(s) removed.`);
+  }
+
+  function initInvitationBlocks() {
+    const blocks = queryInvitationBlocks();
+    blocks.forEach((block) => {
+      if (!block.getAttribute('contenteditable')) {
+        block.setAttribute('contenteditable', 'false');
+      }
+    });
+  }
+
+  const divMessage = document.querySelector('.rc-message');
+  function showMessage(message) {
     if (divMessage) {
-        const timestamp = (new Date()).toLocaleTimeString();
-        divMessage.innerHTML = `<span class="rc-timestamp">${timestamp}</span><p>${message}</p>`;
+      const timestamp = (new Date()).toLocaleTimeString();
+      divMessage.innerHTML = `<span class="rc-timestamp">${timestamp}</span><p>${message}</p>`;
     }
-}
+  }
 
-const btnSchedule = document.querySelector('.rc-schedule-meeting');
-if (btnSchedule) {
-    btnSchedule.addEventListener('click', () => {
-        if (richEditor) {
-            appendHtml(richEditor, meetingInvitation);
-            showMessage('1 Meeting block injected');
-        }
+  const btnScheduleInvitation = document.querySelector('.rc-schedule-invitation');
+  if (btnScheduleInvitation) {
+    btnScheduleInvitation.addEventListener('click', () => {
+      if (richEditor) {
+        appendHtml(richEditor, meetingInvitation);
+        initInvitationBlocks();
+        showMessage('1 invitation block injected');
+      }
     });
-}
+  }
 
-const btnClean = document.querySelector('.rc-clean-meeting');
-if (btnClean) {
-    btnClean.addEventListener('click', () => {
-        const blocks = queryInvitationBlocks();
-        blocks.forEach((block) => {
-            block.parentNode.removeChild(block);
+  const btnCleanInvitation = document.querySelector('.rc-clean-invitation');
+  if (btnCleanInvitation) {
+    btnCleanInvitation.addEventListener('click', () => {
+      cleanInvitationBlocks();
+    });
+  }
+
+  function injectRemoveInvitationButton() {
+    let removeBtn = document.querySelector('.rc-remove-invitation');
+    if (!removeBtn) {
+      const ul = document.querySelector('.popupPanel ul');
+      insertBefore(ul.children[2], '<li><button class="rc-remove-invitation">Remove Invitation</button></li>');
+      removeBtn = document.querySelector('.rc-remove-invitation');
+      if (removeBtn) {
+        removeBtn.addEventListener('click', () => {
+          cleanInvitationBlocks();
         });
-        showMessage(`${blocks.length} meeting block(s) removed.`);
-    });
-}
+      }
+    }
+  }
 
-const observer = new MutationObserver((mutationsList, observer) => {
+  const observer = new MutationObserver((mutationsList, observer) => {
     richEditor = document.querySelector('[role="textbox"][contenteditable="true"]');
     if (!richEditor) {
-        showMessage('Meeting details closed');
-        return;
+      showMessage('Calendar details closed');
+      return;
     }
     if (!richEditor.getAttribute('rc-binded')) {
-        richEditor.setAttribute('rc-binded', 'true');
-        const blocks = queryInvitationBlocks();
-        blocks.forEach((block) => {
-            initInvitationBlock(block);
-        });
-        showMessage(`${blocks.length} meeting block(s) found`);
+      richEditor.setAttribute('rc-binded', 'true');
+      initInvitationBlocks();
+      injectRemoveInvitationButton();
+      showMessage('Calendar details initialized');
     }
-});
+  });
 
-observer.observe(document.body, {
-    // attributes: true,
+  observer.observe(document.body, {
+    attributes: false,
     childList: true,
-    subtree: true,
-});
+    subtree: false,
+  });
+
+  let mouseMoveTimeoutId;
+  const throttlingTime = 256;
+  const removeCloseDelay = 256;
+  function detectCloseButton(ev) {
+    clearTimeout(mouseMoveTimeoutId);
+    mouseMoveTimeoutId = setTimeout(() => {
+      queryInvitationBlocks().forEach((block) => {
+        if (block === ev.target || block.contains(ev.target)) {
+          clearTimeout(block.removeCloseTimeoutId);
+          let closeBtn = block.querySelector('.ringcentral-invitation-close');
+          if (!closeBtn) {
+            const header = block.querySelector('[aria-label="ringcentral-invitation-header"]');
+            appendHtml(header, '<a class="ringcentral-invitation-close">X</a>');
+            closeBtn = block.querySelector('.ringcentral-invitation-close');
+            closeBtn.addEventListener('click', () => {
+              block.parentNode.removeChild(block);
+            });
+          }
+        } else {
+          const closeBtn = block.querySelector('.ringcentral-invitation-close');
+          if (closeBtn) {
+            block.removeCloseTimeoutId = setTimeout(() => {
+              closeBtn.parentNode.removeChild(closeBtn);
+            }, removeCloseDelay);
+          }
+        }
+      });
+    }, throttlingTime);
+  }
+  document.addEventListener('mousemove', detectCloseButton);
+}
+
+window.addEventListener('load', initPOC);
